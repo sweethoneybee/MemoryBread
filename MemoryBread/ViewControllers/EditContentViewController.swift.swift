a2f7511a664daf00a65955c68485595f28bb6660
sweethoneybee <jsjphone8@gmail.com>
2021-11-06 22:50:41 +0900

    콘텐츠편집창 키보드가 편집창을 가리는 이슈 해결
    iOS 14에서는 이상하게 UIResponder.keyboardWillShow가 보내준 키보드 높이값이
    자꾸 0으로 잡히던 이슈가 있었다고 기억했는데 iOS 지금은 안그래서.. 이상하긴 한데
    WillShow가 DidShow보다 동작이 자연스러워서 사용함.

--- MemoryBread/ViewControllers/EditContentViewController.swift.swift
+++ MemoryBread/ViewControllers/EditContentViewController.swift.swift
@@ -10,13 +10,16 @@ import SnapKit
 
 final class EditContentViewController: UIViewController {
     struct UIConstants {
-        static let inset = 20
+        static let contentInset: CGFloat = 20
     }
     
     var content: String
     var didCompleteEditing: ((String) -> (Void))?
     
     private var contentTextField: UITextView!
+    private var bottomConstraint: Constraint?
+    private var keyboardShown = false
+    
     required init?(coder: NSCoder) {
         fatalError("not implemented")
     }
@@ -32,6 +35,9 @@ final class EditContentViewController: UIViewController {
         
         configureHierarchy()
         configureNavigation()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -43,7 +49,10 @@ final class EditContentViewController: UIViewController {
 extension EditContentViewController {
     private func configureHierarchy() {
         contentTextField = UITextView().then {
+            $0.adjustsFontForContentSizeCategory = true
+            $0.font = .preferredFont(forTextStyle: .body)
             $0.text = content
+            $0.contentInset = UIEdgeInsets(top: 0, left: UIConstants.contentInset, bottom: 0, right: UIConstants.contentInset)
         }
         
         view.addSubview(contentTextField)
@@ -52,8 +61,8 @@ extension EditContentViewController {
     
     private func configureLayouts() {
         contentTextField.snp.makeConstraints { make in
-            make.top.bottom.equalTo(view.safeAreaLayoutGuide)
-            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(UIConstants.inset)
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            bottomConstraint = make.bottom.equalTo(view.safeAreaLayoutGuide).constraint
         }
     }
     
@@ -75,7 +84,7 @@ extension EditContentViewController {
 }
 
 // MARK: - objc
-extension EditContentViewController: UITextViewDelegate {
+extension EditContentViewController {
     @objc func cancelEditing() {
         dismiss(animated: true)
     }
@@ -84,5 +93,40 @@ extension EditContentViewController: UITextViewDelegate {
     private func completeEditing() {
         didCompleteEditing?(contentTextField.text)
         dismiss(animated: true)
+    }
+}
+
+// MARK: - Keyboard Show, Hide
+extension EditContentViewController {
+    // refer to sendbird SDK example
+    @objc
+    private func keyboardWillShow(_ notification: Notification) {
+        guard let keyboardFrameEnd = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardFrameEndRect = keyboardFrameEnd.cgRectValue
+        let keyboardHeightWithoutSafeLayoutInset = keyboardFrameEndRect.height - view.safeAreaInsets.bottom
+        if !keyboardShown {
+            UIView.animate(withDuration: 0.3,
+                           delay: 0,
+                           options: .curveEaseInOut,
+                           animations: {
+                self.bottomConstraint?.update(offset: -keyboardHeightWithoutSafeLayoutInset)
+                self.view.layoutIfNeeded()
+            })
+        }
+        keyboardShown = true
+    }
+    
+    @objc
+    private func keyboardWillHide(_ notification: Notification) {
+        if keyboardShown {
+            UIView.animate(withDuration: 0.3,
+                           delay: 0,
+                           options: .curveEaseInOut,
+                           animations: {
+                self.bottomConstraint?.update(offset: 0)
+                self.view.layoutIfNeeded()
+            })
+        }
+        keyboardShown = false
     }
 }
