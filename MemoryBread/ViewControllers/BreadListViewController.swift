9d089ad00c4f63b89f90ba63bb3c5cd303d78989
sweethoneybee <jsjphone8@gmail.com>
2022-02-02 17:27:29 +0900

    BreadListViewController에서 뷰 분리

--- MemoryBread/ViewControllers/BreadListViewController.swift
+++ MemoryBread/ViewControllers/BreadListViewController.swift
@@ -11,27 +11,19 @@ import SnapKit
 import CoreData
 
 final class BreadListViewController: UIViewController {
-    
-    static let reuseIdentifier = "reuse-identifier-bread-list-view"
 
     // MARK: - Views
-    private var tableView: UITableView!
-    private var headerLabel: UILabel!
-    private var addBreadButton: UIButton!
+    private lazy var mainView = BreadListView()
     
     private var remoteDriveItem: UIBarButtonItem!
     private var moresItem: UIBarButtonItem!
     private var doneItem: UIBarButtonItem!
     
-    private var bottomToolbar: BottomToolbar!
-    private var deleteButton: UIButton!
-    private var deleteAllButton: UIButton!
-    
     private var normalRightBarButtonItems: [UIBarButtonItem] {
         return [moresItem, remoteDriveItem]
     }
     
-    private var editRightBarButtonItems: [UIBarButtonItem] {
+    private var editingRightBarButtonItems: [UIBarButtonItem] {
         return [doneItem]
     }
     
@@ -67,6 +59,12 @@ final class BreadListViewController: UIViewController {
     private var diffableDataSource: UITableViewDiffableDataSource<Int, NSManagedObjectID>!
 
     // MARK: - Life Cycle
+    override func loadView() {
+        self.view = mainView
+        self.view.backgroundColor = .systemBackground
+        self.view.tintColor = .systemPink
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("not implemented")
     }
@@ -78,84 +76,34 @@ final class BreadListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setViews()
+        setNavigationItem()
         configureDataSource()
-        tableView.delegate = self
+        
+        mainView.delegate = self
+        mainView.tableView.delegate = self
         
         try? fetchedResultsController.performFetch()
     }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+
+        if isTableViewSwipeActionShowing {
+            isTableViewSwipeActionShowing = false
+            mainView.tableView.setEditing(false, animated: animated)
+        }
+        
+        mainView.tableView.setEditing(editing, animated: animated)
+        mainView.updateUI(for: .init(isEditing: editing, numberOfSelectedRows: 0))
+        
+        navigationItem.title = folderName
+        navigationItem.hidesBackButton = editing
+        navigationItem.rightBarButtonItems = editing ? editingRightBarButtonItems : normalRightBarButtonItems
+    }
 }
 
-// MARK: - Configure Views
+// MARK: - Configure Navigation
 extension BreadListViewController {
-    private func setViews() {
-        view.backgroundColor = .systemBackground
-        
-        tableView = UITableView(frame: .zero, style: .insetGrouped).then {
-            $0.allowsMultipleSelectionDuringEditing = true
-            $0.tintColor = .systemPink
-            $0.contentInset.bottom = 40
-        }
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: BreadListViewController.reuseIdentifier)
-        view.addSubview(tableView)
-
-        headerLabel = UILabel().then {
-            $0.font = .systemFont(ofSize: 14, weight: .light)
-            $0.textAlignment = .center
-            $0.frame.size.height = 30
-            tableView.tableHeaderView = $0
-        }
-        
-        addBreadButton = UIButton().then {
-            $0.setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
-            $0.tintColor = .systemPink
-            $0.addTarget(self, action: #selector(addBreadButtonTouched), for: .touchUpInside)
-        }
-        view.addSubview(addBreadButton)
-        
-        bottomToolbar = BottomToolbar(frame: .init(x: 0, y: 0, width: 200, height: 100)).then {
-            $0.isHidden = true
-        }
-        view.addSubview(bottomToolbar)
-        
-        deleteButton = UIButton(type: .system).then {
-            $0.setTitle(LocalizingHelper.delete, for: .normal)
-            $0.tintColor = .systemPink
-            $0.addTarget(self, action: #selector(deleteButtonTouched), for: .touchUpInside)
-            bottomToolbar.addArrangedSubview($0, to: .right)
-        }
-        
-        deleteAllButton = UIButton(type: .system).then {
-            $0.setTitle(LocalizingHelper.deleteAll, for: .normal)
-            $0.tintColor = .systemPink
-            $0.addTarget(self, action: #selector(deleteAllButtonTouched), for: .touchUpInside)
-            bottomToolbar.addArrangedSubview($0, to: .right)
-        }
-        
-        configureHierarchy()
-        setNavigationItem()
-    }
-    
-    private func configureHierarchy() {
-        tableView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        addBreadButton.snp.makeConstraints { make in
-            make.size.equalTo(CGSize(width: 60, height: 60))
-            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-20)
-            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-20)
-        }
-        addBreadButton.imageView?.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        bottomToolbar.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-50)
-            make.bottom.equalToSuperview()
-            make.leading.trailing.equalToSuperview()
-        }
-    }
     
     private func setNavigationItem() {
         navigationItem.title = folderName ?? LocalizingHelper.appTitle
@@ -187,7 +135,65 @@ extension BreadListViewController {
     
     // MARK: - UIButton Target Actions
     @objc
-    func addBreadButtonTouched() {
+    func remoteDriveItemTouched() {
+        let rdaVC = RemoteDriveAuthViewController(context: coreDataStack.writeContext)
+        rdaVC.folderObjectID = folderObjectID
+        let nvc = UINavigationController(rootViewController: rdaVC)
+        present(nvc, animated: true)
+    }
+    
+    @objc
+    func moresItemTouched() {
+        setEditing(true, animated: true)
+    }
+    
+    @objc
+    func doneItemTouched() {
+        setEditing(false, animated: true)
+    }
+}
+
+// MARK: - DataSource
+extension BreadListViewController {
+    class DataSource: UITableViewDiffableDataSource<Int, NSManagedObjectID> {
+        override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+            return true
+        }
+    }
+    
+    private func configureDataSource() {
+        let titleAttribute = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)]
+        let dateAttribute = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .ultraLight)]
+        let bodyAttribute = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .light)]
+        
+        let dateHelper = DateHelper()
+        diffableDataSource = DataSource(tableView: mainView.tableView) { [weak self] tableView, indexPath, objectID in
+            guard let object = try? self?.viewContext.existingObject(with: objectID) as? Bread else {
+                fatalError("Managed object should be available")
+            }
+            let cell = tableView.dequeueReusableCell(withIdentifier: BreadListView.cellReuseIdentifier, for: indexPath)
+            
+            var content = cell.defaultContentConfiguration()
+            let titleAttributedString = NSAttributedString(string: object.title ?? "", attributes: titleAttribute)
+            content.attributedText = titleAttributedString
+            content.textProperties.numberOfLines = 1
+            
+            let dateString = dateHelper.string(from: object.touch ?? Date())
+            let secondaryAttributedString = NSMutableAttributedString(string: dateString + " ", attributes: dateAttribute)
+            secondaryAttributedString.append(NSAttributedString(string: String((object.content ?? "").prefix(200)), attributes: bodyAttribute))
+            
+            content.secondaryAttributedText = secondaryAttributedString
+            content.secondaryTextProperties.numberOfLines = 1
+            
+            cell.contentConfiguration = content
+            return cell
+        }
+    }
+}
+
+// MARK: - BreadListViewDelegate
+extension BreadListViewController: BreadListViewDelegate {
+    func createBreadButtonTouched() {
         guard isAdding == false else {
             return
         }
@@ -217,130 +223,33 @@ extension BreadListViewController {
         }
     }
     
-    @objc
-    func remoteDriveItemTouched() {
-        let rdaVC = RemoteDriveAuthViewController(context: coreDataStack.writeContext)
-        rdaVC.folderObjectID = folderObjectID
-        let nvc = UINavigationController(rootViewController: rdaVC)
-        present(nvc, animated: true)
-    }
-    
-    @objc
-    func moresItemTouched() {
-        setTableViewEditing(true, animated: true)
-    }
-    
-    @objc
-    func doneItemTouched() {
-        setTableViewEditing(false, animated: true)
-    }
-    
-    @objc
-    func deleteButtonTouched() {
-        if let rows = tableView.indexPathsForSelectedRows {
-            let alertSheet = BasicAlert.makeDestructiveAlertSheet(alertTitle: String(format: LocalizingHelper.selectedNumberOfItems, rows.count), destructiveTitle: LocalizingHelper.deleteSelectedMemoryBread) { [weak self] _ in
+    func deleteButtonTouched(selectedIndexPaths rows: [IndexPath]?) {
+        guard let rows = rows else {
+            return
+        }
+        
+        let alertSheet = BasicAlert.makeDestructiveAlertSheet(
+            alertTitle: String(format: LocalizingHelper.selectedNumberOfItems, rows.count),
+            destructiveTitle: LocalizingHelper.deleteSelectedMemoryBread,
+            completionHandler: { [weak self] _ in
                 let objectIDs = rows.compactMap {
-                    self?.fetchedResultsController.object(at: $0).objectID
+                    self?.fetchedResultsController.object(at:$0).objectID
                 }
                 self?.coreDataStack.deleteAndSaveObjects(of: objectIDs)
-                self?.setTableViewEditing(false, animated: true)
+                self?.setEditing(false, animated: true)
             }
-            present(alertSheet, animated: true)
-        }
+        )
+        present(alertSheet, animated: true)
     }
     
-    @objc
     func deleteAllButtonTouched() {
         let actionSheet = BasicAlert.makeDestructiveAlertSheet(destructiveTitle: LocalizingHelper.deleteAll) { [weak self] _ in
             if let objectIDs = self?.fetchedResultsController.fetchedObjects?.map({ $0.objectID }) {
                 self?.coreDataStack.deleteAndSaveObjects(of: objectIDs)
-                self?.setTableViewEditing(false, animated: true)
+                self?.setEditing(false, animated: true)
             }
         }
         present(actionSheet, animated: true)
-    }
-}
-
-// MARK: - Update Views
-extension BreadListViewController {
-    private func setTableViewEditing(_ editing: Bool, animated: Bool) {
-        
-        if isTableViewSwipeActionShowing {
-            isTableViewSwipeActionShowing = false
-            tableView.setEditing(false, animated: animated)
-        }
-        
-        tableView.setEditing(editing, animated: animated)
-        updateViewsInEditMode(withCount: 0)
-        if animated {
-            let animation = UIViewPropertyAnimator(duration: 0.2, curve: .easeOut) {
-                self.updateUI(whenEditing: editing)
-            }
-            animation.startAnimation()
-        } else {
-            updateUI(whenEditing: editing)
-        }
-    }
-    
-    private func updateUI(whenEditing editing: Bool) {
-        bottomToolbar.isHidden = !editing
-        addBreadButton.isHidden = editing
-        navigationItem.hidesBackButton = editing
-        
-        if editing {
-            bottomToolbar.layer.opacity = 1
-            addBreadButton.layer.opacity = 0
-            navigationItem.rightBarButtonItems = editRightBarButtonItems
-        } else {
-            bottomToolbar.layer.opacity = 0
-            addBreadButton.layer.opacity = 1
-            navigationItem.rightBarButtonItems = normalRightBarButtonItems
-        }
-    }
-    
-    private func updateViewsInEditMode(withCount numberOfSelectedRows: Int) {
-        let hasSelectedRows = (numberOfSelectedRows != 0)
-        deleteButton.isHidden = !hasSelectedRows
-        deleteAllButton.isHidden = hasSelectedRows
-        navigationItem.title = hasSelectedRows ? String(format: LocalizingHelper.selectedNumberOfItems, numberOfSelectedRows) : folderName
-    }
-}
-
-// MARK: - DataSource
-extension BreadListViewController {
-    class DataSource: UITableViewDiffableDataSource<Int, NSManagedObjectID> {
-        override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-            return true
-        }
-    }
-    
-    private func configureDataSource() {
-        let titleAttribute = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)]
-        let dateAttribute = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .ultraLight)]
-        let bodyAttribute = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .light)]
-        
-        let dateHelper = DateHelper()
-        diffableDataSource = DataSource(tableView: tableView) { [weak self] tableView, indexPath, objectID in
-            guard let object = try? self?.viewContext.existingObject(with: objectID) as? Bread else {
-                fatalError("Managed object should be available")
-            }
-            let cell = tableView.dequeueReusableCell(withIdentifier: BreadListViewController.reuseIdentifier, for: indexPath)
-            
-            var content = cell.defaultContentConfiguration()
-            let titleAttributedString = NSAttributedString(string: object.title ?? "", attributes: titleAttribute)
-            content.attributedText = titleAttributedString
-            content.textProperties.numberOfLines = 1
-            
-            let dateString = dateHelper.string(from: object.touch ?? Date())
-            let secondaryAttributedString = NSMutableAttributedString(string: dateString + " ", attributes: dateAttribute)
-            secondaryAttributedString.append(NSAttributedString(string: String((object.content ?? "").prefix(200)), attributes: bodyAttribute))
-            
-            content.secondaryAttributedText = secondaryAttributedString
-            content.secondaryTextProperties.numberOfLines = 1
-            
-            cell.contentConfiguration = content
-            return cell
-        }
     }
 }
 
@@ -348,9 +257,15 @@ extension BreadListViewController {
 extension BreadListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if tableView.isEditing {
-            updateViewsInEditMode(withCount: tableView.indexPathsForSelectedRows?.count ?? 0)
+            let indexPaths = tableView.indexPathsForSelectedRows
+            mainView.updateUI(for: .init(
+                isEditing: true,
+                numberOfSelectedRows: indexPaths?.count ?? 0
+            ))
+            navigationItem.title = (indexPaths != nil) ? String(format: LocalizingHelper.selectedNumberOfItems, indexPaths!.count) : folderName
             return
         }
+        
         let breadVC = BreadViewController(context: viewContext, bread: fetchedResultsController.object(at: indexPath))
         navigationController?.pushViewController(breadVC, animated: true)
         tableView.deselectRow(at: indexPath, animated: true)
@@ -358,7 +273,12 @@ extension BreadListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         if tableView.isEditing {
-            updateViewsInEditMode(withCount: tableView.indexPathsForSelectedRows?.count ?? 0)
+            let indexPaths = tableView.indexPathsForSelectedRows
+            mainView.updateUI(for: .init(
+                isEditing: true,
+                numberOfSelectedRows: indexPaths?.count ?? 0
+            ))
+            navigationItem.title = (indexPaths != nil) ? String(format: LocalizingHelper.selectedNumberOfItems, indexPaths!.count) : folderName
             return
         }
     }
@@ -393,13 +313,13 @@ extension BreadListViewController: UITableViewDelegate {
 // MARK: - NSFetchedResultsControllerDelegate
 extension BreadListViewController: NSFetchedResultsControllerDelegate {
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChangeContentWith snapshot: NSDiffableDataSourceSnapshotReference) {
-        guard let dataSource = tableView.dataSource as? UITableViewDiffableDataSource<Int, NSManagedObjectID> else {
+        guard let dataSource = mainView.tableView.dataSource as? UITableViewDiffableDataSource<Int, NSManagedObjectID> else {
             assertionFailure("The data source has not implemented snapshot support while it should")
             return
         }
-        let shouldAnimate = tableView.numberOfSections != 0
+        let shouldAnimate = mainView.tableView.numberOfSections != 0
         dataSource.apply(snapshot as NSDiffableDataSourceSnapshot<Int, NSManagedObjectID>, animatingDifferences: shouldAnimate)
-        headerLabel.text = String(format: LocalizingHelper.numberOfMemoryBread, snapshot.numberOfItems)
+        mainView.tableViewHeaderLabel.text = String(format: LocalizingHelper.numberOfMemoryBread, snapshot.numberOfItems)
     }
 }
 
---
+++ MemoryBread/Views/CustomViews/BreadListView.swift
@@ -0,0 +1,138 @@
+//
+//  BreadListView.swift
+//  MemoryBread
+//
+//  Created by 정성훈 on 2022/02/02.
+//
+
+import UIKit
+
+protocol BreadListViewDelegate: AnyObject {
+    func createBreadButtonTouched()
+    func deleteButtonTouched(selectedIndexPaths rows: [IndexPath]?)
+    func deleteAllButtonTouched()
+}
+
+final class BreadListView: UIView {
+
+    static let cellReuseIdentifier = "bread-list-view-cell-reuse-identifier"
+    
+    weak var delegate: BreadListViewDelegate?
+    
+    let tableView = UITableView(frame: .zero, style: .insetGrouped).then {
+        $0.allowsMultipleSelectionDuringEditing = true
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: BreadListView.cellReuseIdentifier)
+    }
+
+    let tableViewHeaderLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14, weight: .thin)
+        $0.textAlignment = .center
+        $0.frame.size.height = 30
+    }
+    
+    let createBreadButton = UIButton(type: .system).then {
+        $0.setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
+    }
+    
+    let bottomToolbar = BottomToolbar(frame: .init(x: 0, y: 0, width: 200, height: 100)).then {
+        $0.isHidden = true
+    }
+    
+    let bottomDeleteButton = UIButton(type: .system).then {
+        $0.setTitle(LocalizingHelper.delete, for: .normal)
+    }
+    
+    @objc let bottomDeleteAllButton = UIButton(type: .system).then {
+        $0.setTitle(LocalizingHelper.deleteAll, for: .normal)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+        configureLayouts()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUp()
+        configureLayouts()
+    }
+}
+
+extension BreadListView {
+    func updateUI(for state: State) {
+        createBreadButton.isHidden = state.isEditing
+        bottomToolbar.isHidden = !state.isEditing
+        
+        bottomDeleteButton.isHidden = (state.numberOfSelectedRows == 0)
+        bottomDeleteAllButton.isHidden = !(bottomDeleteButton.isHidden)
+        
+        if state.isEditing {
+            createBreadButton.layer.opacity = 0
+            bottomToolbar.layer.opacity = 1
+        } else {
+            createBreadButton.layer.opacity = 1
+            bottomToolbar.layer.opacity = 0
+        }
+    }
+    
+    private func setUp() {
+        addSubview(tableView)
+        addSubview(createBreadButton)
+        addSubview(bottomToolbar)
+        
+        tableView.tableHeaderView = tableViewHeaderLabel
+        bottomToolbar.addArrangedSubview(bottomDeleteButton, to: .right)
+        bottomToolbar.addArrangedSubview(bottomDeleteAllButton, to: .right)
+        
+        createBreadButton.addTarget(self, action: #selector(createBreadButtonTouched), for: .touchUpInside)
+        bottomDeleteButton.addTarget(self, action: #selector(bottomDeleteButtonTouched), for: .touchUpInside)
+        bottomDeleteAllButton.addTarget(self, action: #selector(bottomDeleteAllButtonTouched), for: .touchUpInside)
+    }
+    
+    private func configureLayouts() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        createBreadButton.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: 60, height: 60))
+            make.trailing.equalTo(safeAreaLayoutGuide.snp.trailing).offset(-20)
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).offset(-20)
+        }
+        
+        createBreadButton.imageView?.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: 60, height: 60))
+        }
+        
+        bottomToolbar.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.bottom).offset(-50)
+            make.bottom.equalToSuperview()
+            make.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+}
+
+extension BreadListView {
+    @objc
+    func createBreadButtonTouched() {
+        delegate?.createBreadButtonTouched()
+    }
+    
+    @objc
+    func bottomDeleteButtonTouched() {
+        delegate?.deleteButtonTouched(selectedIndexPaths: tableView.indexPathsForSelectedRows)
+    }
+    
+    @objc
+    func bottomDeleteAllButtonTouched() {
+        delegate?.deleteAllButtonTouched()
+    }
+}
+
+extension BreadListView {
+    struct State {
+        let isEditing: Bool
+        let numberOfSelectedRows: Int
+    }
+}
