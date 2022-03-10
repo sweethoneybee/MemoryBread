f1424a40001100e7c97f1d8accfff92bd354a36b
sweethoneybee <jsjphone8@gmail.com>
2022-03-10 20:08:44 +0900

    암기빵 내 한 단어가 너무 긴 경우 임의로 쪼개는 로직 추가, String Extension 출처 수정

--- MemoryBread/Extensions/String+Helper.swift
+++ MemoryBread/Extensions/String+Helper.swift
@@ -7,16 +7,32 @@
 
 import UIKit
 
-// from https://stackoverflow.com/questions/30450434/figure-out-size-of-uilabel-based-on-string-in-swift
 extension String {
     var localized: String {
         return NSLocalizedString(self, tableName: "Localizable", comment: "")
     }
-    
+}
+
+extension String {
+    /// from https://stackoverflow.com/questions/30450434/figure-out-size-of-uilabel-based-on-string-in-swift
+    /// by Kaan Dedeoglu
     func height(withConstraintWidth width: CGFloat, font: UIFont) -> CGFloat {
         let constraintBox = CGSize(width: width, height: .greatestFiniteMagnitude)
         let boundingBox = self.boundingRect(with: constraintBox, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
         
         return ceil(boundingBox.height)
+    }
+}
+
+extension String {
+    /// from https://stackoverflow.com/questions/32212220/how-to-split-a-string-into-substrings-of-equal-length
+    /// by Wujo
+    func components(withMaxLength length: Int) -> [String] {
+        return stride(from: 0, to: self.count, by: length)
+            .map {
+                let start = self.index(startIndex, offsetBy: $0)
+                let end = self.index(start, offsetBy: length, limitedBy: endIndex) ?? endIndex
+                return String(self[start..<end])
+            }
     }
 }
